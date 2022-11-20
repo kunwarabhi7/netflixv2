0a5989f9cc9cc0a326e69fcec74b2688b3c60b9b
Abhishek <abhishekparmar162@gmail.com>
2022-11-20 14:06:35 +0530

    added background image

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,15 +1,22 @@
 import Image from 'next/image'
 import Logo from '../public/assests/logo.png'
+import {GiEarthAmerica} from 'react-icons/gi'
+import {IoMdArrowDropdown} from 'react-icons/io'
 
 const NavBar = () => {
   return (
     <div className='h-[180px] m-4 w-full flex justify-between'>
         <div>
 
-<Image src={Logo} height='45' width='167'/>
+<Image src={Logo} height='45' width='167' className='block cursor-pointer'/>
         </div>
-<div className='text-white mr-16'>
-    <button className='bg-transparent border  border-white py-2 px-5 mr-8' >Language</button>
+<div className='text-white mt-5 mr-16 '>
+    
+    <button className='bg-transparent border  border-white py-2 px-5 mr-8' >
+       <div className='flex items-center'>
+         <GiEarthAmerica/> English <IoMdArrowDropdown/>
+        </div>
+        </button>
     <button className='bg-red-700 text-white rounded-xl py-3 h-12 w-18  px-6 cursor-pointer'>Sign In</button>
 </div>
     </div>
