7970e30fed9260ee7e3816a7732f15b35cb0abe6
Abhishek <abhishekparmar162@gmail.com>
2022-11-20 15:44:17 +0530

    bg color set to black

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Logo from '../public/assests/logo.png'
 
 const login = () => {
@@ -7,7 +8,9 @@ const login = () => {
 <Image src='https://assets.nflxext.com/ffe/siteui/vlv3/5aecc44d-2a1f-4313-8399-98df20908b64/4d9d5abf-07d8-4dd6-9899-a96b902312ee/IN-en-20221114-popsignuptwoweeks-perspective_alpha_website_large.jpg' width={1640} height='800'  />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen '>
         </div>
+        <Link href='/'>
         <Image src={Logo} width='250' height={100} className='absolute top-0 left-10'  />
+        </Link>
         <div className='absolute bg-black/75  w-[450px] h-[600px] left-[36rem] top-40 mx-auto text-white'>
             <p className='ml-16 mt-8 text-3xl text-white'>Sign In</p>
             <div className='flex flex-col m-8 mb-4 '>
