96086e5af6aaa922a28ddcc11bd6d1a9a26c0c02
Abhishek <abhishekparmar162@gmail.com>
2022-11-20 13:35:07 +0530

    added buttons

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,11 +3,14 @@ import Logo from '../public/assests/logo.png'
 
 const NavBar = () => {
   return (
-    <div className='h-[180px] w-full flex justify-between'>
+    <div className='h-[180px] m-4 w-full flex justify-between'>
+        <div>
+
 <Image src={Logo} height='45' width='167'/>
-<div className='text-white'>
-    <button>Language</button>
-    <button>SignIN</button>
+        </div>
+<div className='text-white mr-16'>
+    <button className='bg-transparent border  border-white py-2 px-5 mr-8' >Language</button>
+    <button className='bg-red-700 text-white rounded-xl py-3 h-12 w-18  px-6 cursor-pointer'>Sign In</button>
 </div>
     </div>
   )
